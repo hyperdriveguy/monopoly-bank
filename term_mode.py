f392ea42423b1a450232a54991522558301b38fd
Carson <hyperdriveguy@gmail.com>
2022-05-30 13:24:37 -0600

    Split out terminal interface

--- term_mode.py
+++ term_mode.py
@@ -1,67 +1,13 @@
 #!/usr/bin/env python
+
+from account_store import AccountManager
+
 MAX_SWIPE_TRIES = 3
 
 def clear(prompt=False):
     if prompt == True:
         input('Press Enter to continue...')
     print('\033c')
-
-
-class Account:
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.cash = 0
-        self.properties = []
-    
-    def __str__(self):
-        return f'{self.name}:\n\tID: {self.id}\n\tBalance: ${self.cash}'
-
-    def withdraw(self, amount):
-        if amount > self.cash:
-            amount = self.cash
-            self.cash = 0
-        else:
-            self.cash -= amount
-        return amount
-
-    def deposit(self, amount):
-        self.cash += amount
-        return amount
-
-
-class AccountManager:
-
-    def __init__(self):
-        self.card_numbers = dict()
-
-    def new(self, id_num, card_holder):
-        """
-        Create a new account
-        """
-        if self.exists(id_num):
-            return 'Card already associated with an account!'
-        self.card_numbers[id_num] = Account(id_num, card_holder)
-        return f'Created new account for {card_holder}.'
-
-    def delete(self, id_num):
-        self.card_numbers.pop(id_num)
-    
-    def exists(self, id_num):
-        return id_num in self.card_numbers
-    
-    def query(self, id_num):
-        return self.card_numbers[id_num]
-    
-    def transfer(self, payer, payee, amount: int):
-        paying_account = self.card_numbers[payer]
-        if paying_account.cash < amount:
-            return f'{paying_account.name} does not have enough funds to complete the transaction.'
-        paying_account.withdraw(amount)
-        payee_account = self.card_numbers[payee]
-        payee_account.deposit(amount)
-        return f'{self.card_numbers[payer].name} paid {self.card_numbers[payee].name} ${amount}.'
 
 
 def term_swipe_card(accounts, verify=True):
@@ -180,7 +126,6 @@ def terminal_mode():
             continue
         all_commands[command](accounts)
 
-    
 
 if __name__ == '__main__':
     terminal_mode()
