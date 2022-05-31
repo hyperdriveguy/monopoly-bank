aa3ff3e737c6505bf00d6b5c9c64f5400d98c7b9
Carson <hyperdriveguy@gmail.com>
2022-05-31 01:28:17 -0600

    Convert to jinja template

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,29 @@ from markupsafe import escape
 
 from account_store import AccountManager
 
+URLS = {
+    'Accounts': 'accounts',
+    'Transfer': 'transfer',
+    'Change Cash': 'change-cash',
+    'Properties': 'properties',
+    'Investments': 'investments',
+    'Auctions': 'auctions',
+    'Help': 'help'
+}
+
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route("/hello/<name>")
-def hello_world(name=None):
-    # return f"<p>Hello, {escape(name)}!</p>"
-    return render_template('hello.html.jinja', name=name)
+
+@app.route('/')
+def home_page():
+    return render_template('home.html.jinja', urls=URLS)
+
+@app.route(f'/{URLS["Accounts"]}')
+@app.route(f'/{URLS["Transfer"]}')
+@app.route(f'/{URLS["Change Cash"]}')
+@app.route(f'/{URLS["Properties"]}')
+@app.route(f'/{URLS["Investments"]}')
+@app.route(f'/{URLS["Auctions"]}')
+@app.route(f'/{URLS["Help"]}')
+def placeholder_page():
+    return render_template('sidebar.html.jinja', urls=URLS)
