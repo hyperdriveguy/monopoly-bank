63aeb62b1401280e4ddcd3561886a53bb733b8f7
Carson <hyperdriveguy@gmail.com>
2022-06-02 02:17:10 -0600

    Implement account searches, show accounts in grid

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
 import random
 
@@ -20,9 +20,20 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html.jinja', urls=URLS)
 
-@app.route(f'/{URLS["Accounts"]}')
-def account_main_page():
-    return render_template('accounts.html.jinja', urls=URLS, accs=managed_accs)
+@app.route(f'/{URLS["Accounts"]}/', methods=['GET', 'POST'])
+def accounts_main_page():
+    # Do account query as applicable
+    if 'account-lookup-query' in request.form:
+        query = request.form['account-lookup-query']
+    else:
+        query = ''
+    lookup = managed_accs.search(query)
+    num_results = len(lookup)
+    return render_template('accounts.html.jinja', urls=URLS, lookup=lookup, query=query, num_results=num_results)
+
+@app.route(f'/{URLS["Accounts"]}/<id>')
+def individual_account_page(id):
+    return str(managed_accs.query(id))
 
 @app.route(f'/{URLS["Transfer"]}')
 @app.route(f'/{URLS["Change Cash"]}')
@@ -38,8 +49,9 @@ if __name__ == 'wsgi':
     managed_accs = AccountManager()
     managed_accs.new('id_num', 'card_holder')
     managed_accs.new('asdf', 'Carson')
-    for _ in range(1000):
-        managed_accs.new(str(random.randint(0,10000)), 'Another Carson')
+    for i in range(100):
+        managed_accs.new(str(random.randint(0,1000000000000000000000000000)), f'{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}{chr((i + random.randint(0, 63)) % 62 + 64)}')
+    print('Server is ready!')
 
 if __name__ == '__main__':
     print('Do not run this file directly with the Python interpreter. Use "flask run" instead.')
