7fd97cef27f1b9af548703df62e4adfbe828d863
Carson <hyperdriveguy@gmail.com>
2022-06-01 19:56:04 -0600

    Make sidebar respect content Use Javascript to get the sidebar size and set content margin (yuck)

---
+++ static/js/set-margin.js
@@ -0,0 +1,8 @@
+function make_margin() {
+    let target_margin = document.querySelector('.side-bar').offsetWidth;
+    // Set left margin of the main section to the width of the sidebar
+    document.querySelector('main').style.margin = `0 0 0 ${target_margin + 10}px`;
+}
+
+window.addEventListener("load", make_margin)
+window.addEventListener("resize", make_margin)
