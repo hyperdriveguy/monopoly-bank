df50254e6d7555b90901239c8dbf8cf39dc94bf4
Carson <hyperdriveguy@gmail.com>
2022-06-04 01:28:01 -0600

    finish account page

---
+++ static/js/accounts.js
@@ -0,0 +1,61 @@
+const new_account_button = document.querySelector('#new-account');
+const new_account_form = document.querySelector('#make-new-acc');
+
+const del_account_button = document.querySelector('#del-account');
+const individual_account_del_buttons = document.querySelectorAll('.indv-acc-del');
+const account_cards = document.querySelectorAll('a.account-card');
+
+const refresh_button = document.querySelector('#refresh');
+
+// Dirty hack to post to server
+function post(path, params, method='post') {
+    const form = document.createElement('form');
+    form.method = method;
+    form.action = path;
+  
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        const hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = key;
+        hiddenField.value = params[key];
+  
+        form.appendChild(hiddenField);
+      }
+    }
+  
+    document.body.appendChild(form);
+    form.submit();
+  }  
+
+new_account_button.addEventListener('click', () => {
+    new_account_form.classList.toggle('hidden');
+    individual_account_del_buttons.forEach(node => {
+        node.classList.add("hidden");
+    });
+    account_cards.forEach(node => {
+        node.classList.add("clickable");
+    })
+});
+
+del_account_button.addEventListener('click', () => {
+    new_account_form.classList.add('hidden');
+    individual_account_del_buttons.forEach(node => {
+        node.classList.toggle("hidden");
+    });
+    account_cards.forEach(node => {
+        node.classList.toggle("clickable");
+    })
+});
+
+individual_account_del_buttons.forEach(node => {
+    node.addEventListener('click', () => {
+        if (confirm(`Delete account with ID "${node.attributes.account.value}"?`)) {
+            post("", {"del-acc-id": node.attributes.account.value});
+        }
+    });
+});
+
+refresh_button.addEventListener('click', () => {
+    window.location=window.location;
+})
