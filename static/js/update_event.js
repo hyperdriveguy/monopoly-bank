ec020399ed8a3c262d240c992bec2df99beaaf18
Carson <hyperdriveguy@gmail.com>
2022-06-04 14:14:56 -0600

    Make server events not auto refresh on transfers

--- static/js/update_event.js
+++ static/js/update_event.js
@@ -1,15 +1,45 @@
+// Forms or other elements that are sensitive to refreshes when unhidden.
+const update_sensitive = [document.querySelector('#make-new-acc'), document.querySelector('#transfer-form')];
+let initial_event_done = false;
+
+function isPostBack() {
+    return document.referrer.indexOf(document.location.href) > -1;
+}
+
+// Don't force refresh if accounts change.
+// If the intention is to create or modify an account,
+// preserving the form is more important.
+function keepFormsAlive() {
+    return update_sensitive.some(element => {
+        if (element == undefined) {
+            return false;
+        }
+        // Check if the sensitive element is hidden
+        if (!element.classList.contains('hidden')) {
+            if (!confirm('Account data has been updated, but you\'re in the middle of creating an account. Refresh the page anyway?')) {
+                return true;
+            }
+        }
+        return false;
+    });
+}
+
 window.addEventListener('load', () => {
+    if (!isPostBack()) {
+        initial_event_done = true;
+    }
+    // Wait 100ms for potential event
+    setTimeout(() => {initial_event_done = true;}, 100);
     const source = new EventSource("/bruh");
     source.onmessage = function(req) {
         res = JSON.parse(req.data)
         if (res['sync']) {
-            // Don't force refresh if accounts change.
-            // If the intention is to create a new account,
-            // preserving the form is more important.
-            if (new_account_form != undefined && !new_account_form.classList.contains('hidden')) {
-                if (!confirm('Account data has been updated, but you\'re in the middle of creating an account. Refresh the page anyway?')) {
-                    return;
-                }
+            if (!initial_event_done) {
+                initial_event_done = true;
+                return;
+            }
+            if (keepFormsAlive()) {
+                return;
             }
             window.location=window.location;
         }
