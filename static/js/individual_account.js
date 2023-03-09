f516bc7b18c9c2fcac6e59e08684d666811f8da7
Carson Bush <hyperdriveguy@gmail.com>
2023-03-08 17:03:52 -0700

    Prevent errors when hiding buttons on account pages

--- static/js/individual_account.js
+++ static/js/individual_account.js
@@ -6,20 +6,26 @@ const withdraw_form = document.querySelector('#withdraw-form');
 const deposit_form = document.querySelector('#deposit-form');
 const transfer_form = document.querySelector('#transfer-form');
 
-do_withdraw_button.addEventListener('click', () => {
-    withdraw_form.classList.toggle('hidden');
-    deposit_form.classList.add('hidden');
-    transfer_form.classList.add('hidden');
-});
+if (do_withdraw_button != null) {
+    do_withdraw_button.addEventListener('click', () => {
+        withdraw_form.classList.toggle('hidden');
+        deposit_form.classList.add('hidden');
+        transfer_form.classList.add('hidden');
+    });
+}
 
-do_deposit_button.addEventListener('click', () => {
-    deposit_form.classList.toggle('hidden');
-    withdraw_form.classList.add('hidden');
-    transfer_form.classList.add('hidden');
-});
+if (do_deposit_button != null) {
+    do_deposit_button.addEventListener('click', () => {
+        deposit_form.classList.toggle('hidden');
+        withdraw_form.classList.add('hidden');
+        transfer_form.classList.add('hidden');
+    });
+}
 
-do_transfer_button.addEventListener('click', () => {
-    transfer_form.classList.toggle('hidden');
-    withdraw_form.classList.add('hidden');
-    deposit_form.classList.add('hidden');
-});
+if (do_transfer_button != null) {
+    do_transfer_button.addEventListener('click', () => {
+        transfer_form.classList.toggle('hidden');
+        withdraw_form.classList.add('hidden');
+        deposit_form.classList.add('hidden');
+    });
+}
